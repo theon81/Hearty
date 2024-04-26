2f2943cf565986937682f19cac28a77a95e24983
Quixxo <ngzeie8161@gmail.com>
2024-04-26 22:31:48 +0700

    path fix

--- js/cursor-trail.js
+++ js/cursor-trail.js
@@ -1,18 +1,28 @@
 const trailer = document.getElementById("trailer");
 
+let prevX = 0;
+let prevY = 0;
+
 const animateTrailer = (e, interacting) => {
-    const x = e.clientX - trailer.offsetWidth / 2,
-        y = e.clientY - trailer.offsetHeight / 2;
-  
+    const x = e.clientX - trailer.offsetWidth / 2;
+    const y = e.clientY - trailer.offsetHeight / 2;
+
+    const distance = Math.sqrt((x - prevX) ** 2 + (y - prevY) ** 2);
+    const duration = Math.min(800, distance * 50); // Adjust 5 as needed for responsiveness
+    
     const keyframes = {
         transform: `translate(${x}px, ${y}px) scale(${interacting ? 8 : 1})`
-    }
-  
-    trailer.animate(keyframes, { 
-        duration: 800, 
-        fill: "forwards" 
+    };
+
+    trailer.animate(keyframes, {
+        duration: duration,
+        fill: "forwards"
     });
-}
+
+    prevX = x;
+    prevY = y;
+
+};
 
 const getTrailerClass = type => {
     switch(type) {
